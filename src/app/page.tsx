f8d039480f5ceaaf57a66f8ad5bf6e4a845a5058
hayato94087 <hayato94087@gmail.com>
2023-06-29 10:51:12 +0900

    add google font to single page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Rampart_One } from "next/font/google";
+
+const RampartOneFont = Rampart_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Home() {
   return (
-    <main className="">
-      テストページ
+    <main className="text-4xl">
+      <div className={RampartOneFont.className}>人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは</div>
     </main>
   )
 }
